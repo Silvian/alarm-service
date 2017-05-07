8a937b8df569ae528677d1b569e934b342dfdf52
silvian <silvian.dragan@gmail.com>
2017-05-07 22:25:54 +0100

    Created new user modal process.

--- alarm_service/static/resources/js/alarm-users.js
+++ alarm_service/static/resources/js/alarm-users.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
 
+    $('#users-error').hide();
 
+    getUsers();
+
+    $('#add-user-button').click(function() {
+        clearUserFields();
+        $('#user-modal-label').html("Add a new user");
+        $('#required-fields-alert').hide();
+        $('#user-modal').modal('show');
+    });
 
 });
+
+
+function getUsers() {
+
+
+
+}
+
+function clearUserFields() {
+
+    $('#user-name').val("");
+    $('#user-email').val("");
+    $('#user-mobile').val("");
+
+}
