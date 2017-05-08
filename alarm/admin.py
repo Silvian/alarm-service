3a843e4c68b95ceba526a7710bc9ec012b83c654
silvian <silvian.dragan@gmail.com>
2017-05-08 22:31:45 +0100

    Enhanced the UserProfile model

--- alarm/admin.py
+++ alarm/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Log, AlarmStateConfiguration
+from .models import Log, AlarmStateConfiguration, UserProfile
 
 admin.site.register(AlarmStateConfiguration)
 admin.site.register(Log)
+admin.site.register(UserProfile)
--- alarm/models.py
+++ alarm/models.py
@@ -51,10 +51,11 @@ class AlarmStateConfiguration(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name='user', null=False)
+    email = models.CharField(max_length=200, null=False, blank=False)
     mobile = models.CharField(max_length=200, null=False, blank=False)
 
     def publish(self):
         self.save()
 
     def __str__(self):
-        return self.user.name
+        return self.email
