dc8aee1c076a60e5d984b54486d7ea380d0d259a
silvian <silvian.dragan@gmail.com>
2017-05-30 20:10:08 +0100

    Added runsslserver to run shell script.

--- cli/run.sh
+++ cli/run.sh
@@ -3,4 +3,4 @@
 sleep 5
 python manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('root', 'root@admin.com', 'root')" | python manage.py shell
-python manage.py runserver 0.0.0.0:8000
+python manage.py runsslserver 0.0.0.0:8000
